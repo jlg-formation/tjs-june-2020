d1f7fc9db16f2588e9c6ce7c70211beb655f1e30
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-09 12:19:43 +0200

    added submit

--- front/src/routes/AppStockNew.tsx
+++ front/src/routes/AppStockNew.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, SyntheticEvent } from "react";
 import "./AppStockNew.scss";
 
 function AppStockNew() {
@@ -6,21 +6,32 @@ function AppStockNew() {
   const [price, setPrice] = useState(12.23);
   const [qty, setQty] = useState(110);
 
+  function submit(e: SyntheticEvent) {
+    e.preventDefault();
+    console.log("submit: ", { name, price, qty });
+  }
+
   return (
     <section className="stock-new">
       <h1>Ajouter un article</h1>
-      <form>
+      <form onSubmit={submit}>
         <label>
           <div>Nom</div>
           <input type="text" onInput={(e) => setName(e.currentTarget.value)} />
         </label>
         <label>
           <div>Prix</div>
-          <input type="number" onInput={(e) => setPrice(+e.currentTarget.value)} />
+          <input
+            type="number"
+            onInput={(e) => setPrice(+e.currentTarget.value)}
+          />
         </label>
         <label>
           <div>Quantité</div>
-          <input type="number" onInput={(e) => setQty(+e.currentTarget.value)} />
+          <input
+            type="number"
+            onInput={(e) => setQty(+e.currentTarget.value)}
+          />
         </label>
         <button type="submit">Créer</button>
       </form>
