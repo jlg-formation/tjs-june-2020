905ad5a59498ebffba726c516ae5e5a0c2ffc2c2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-09 10:53:59 +0200

    adding button in appstock

--- front/src/routes/AppStock.tsx
+++ front/src/routes/AppStock.tsx
@@ -1,34 +1,41 @@
 import React from "react";
 import "./AppStock.css";
+import { Link } from "react-router-dom";
 
 function AppStock() {
   return (
     <section className="stock">
       <h1>Liste d'articles</h1>
+      <div className="buttons">
+        <button>Raffraichir</button>
+        <Link to="/stock/new">
+          <button>Ajouter</button>
+        </Link>
+      </div>
       <table>
         <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Prix</th>
-          <th>Quantité</th>
-        </tr>
+          <tr>
+            <th>Nom</th>
+            <th>Prix</th>
+            <th>Quantité</th>
+          </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Tournevis</td>
-          <td>1.23 €</td>
-          <td>100</td>
-        </tr>
-        <tr>
-          <td>Tournevis</td>
-          <td>1.23 €</td>
-          <td>100</td>
-        </tr>
-        <tr>
-          <td>Tournevis</td>
-          <td>1.23 €</td>
-          <td>100</td>
-        </tr>
+          <tr>
+            <td>Tournevis</td>
+            <td>1.23 €</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Tournevis</td>
+            <td>1.23 €</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>Tournevis</td>
+            <td>1.23 €</td>
+            <td>100</td>
+          </tr>
         </tbody>
       </table>
     </section>
